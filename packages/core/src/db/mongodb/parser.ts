5c423011d9a9e0ca1f7b1ae19492a0a2ef0510b9
Jaeil Yu <jiyu@imagoworks.ai>
2024-05-29 17:17:36 +0900

    test: test ai review

---
+++ packages/core/src/db/mongodb/parser.ts
@@ -0,0 +1,10 @@
+export function mongodbParser() {
+  // it is temporary code for ai code review test.
+  return "mongodb-parser";
+}
+
+function mongodbAdd() {
+  return 1 + 2;
+}
+
+function bad() {}
